497ffc436d2431e3e35a22fd2a0133acda844c6b
ThaiDuong2k6 <inumakitaiyou@gmail.com>
2025-03-12 15:32:44 +0700

    Update calculus.cpp

--- AdvProg_L2-Calculus/calculus.cpp
+++ AdvProg_L2-Calculus/calculus.cpp
@@ -6,6 +6,7 @@ using std::string;
 using std::cout;
 using std::endl;
 using std::stod;
+using namespace std;
 
 
 double mySin(double x);
@@ -20,7 +21,14 @@ double mySqrt(double x);
 ***/
 double myCos(double x) 
 {
-    return 0.0;
+    const int TERMS = 10;
+    double result = 1;
+    double term = 1;
+    for (int n = 1; n <= TERMS; ++n) {
+        term *= -1 * x * x / (2 * n * (2 * n - 1));
+        result += term;
+    }
+    return result;
 }
 
 /***
@@ -31,7 +39,18 @@ double myCos(double x)
 ***/
 double mySin(double x)
 {
-    return 0.0;
+    const int TERMS = 10;
+    double result = 0.0;
+    double term = x;
+    int sign = 1;
+
+    for (int n = 1; n <= TERMS; ++n) {
+        result += sign * term;
+        term *= x * x / ((2 * n) * (2 * n + 1));
+        sign *= -1;
+    }
+
+    return result;
 }
 
 
@@ -45,8 +64,13 @@ double mySqrt(double x) {
     if (x < 0) {
         cout << "Invalid argument" << endl;
         exit(1);
+    double guess = x / 2.0;
+    const double EPSILON = 1e-7;
+    while (fabs(guess * guess - x) > EPSILON) {
+        guess = (guess + x / guess) / 2.0;
+    }
+    return guess;
     }
 
-    
     return 0;
 }
